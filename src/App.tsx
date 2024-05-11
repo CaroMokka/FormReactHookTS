50b6dd526304c79869782519d7e84ba9fe3cd287
carolina <ci.arayagonzalez@gmail.com>
2024-05-11 18:21:58 -0400

    add custom hook form

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import  { useMultistepForm }  from "./useMultistepForm"
+import "./styles/style.css"
+
 function App() {
-  return <h1>holaaaaaa</h1>
+
+  const { steps, step, currentStepIndex, isFirstStep, back, next } = useMultistepForm([<div>One</div>, <div>Two</div>])
+
+  return (
+    <div className="form-wrapper">
+      <form>
+        <div className="form-wrapper__step" >{currentStepIndex + 1}/ {steps.length}</div>
+        {step}
+        <div className="form-wrapper__buttons" >
+          { !isFirstStep && <button  type="button" onClick={back} >Back</button> }
+          <button type="button" onClick={next} >Next</button>
+        </div>
+      </form>
+    </div>
+  )
 
   
 }
